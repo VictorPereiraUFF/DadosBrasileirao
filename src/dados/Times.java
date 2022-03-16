8376e03a27f202d237651ce662d7ad5fe057bb06
Victor Pereira <victor.m.s.pereira.95@gmail.com>
2022-03-16 15:54:22 -0300

    Base do programa

---
+++ src/dados/Times.java
@@ -0,0 +1,55 @@
+package dados;
+
+public class Times {
+
+	private String time;
+	private Integer pontos;
+	private Integer ataque;
+	private Integer defesa;
+	
+	public Times(String time, Integer pontos, Integer ataque, Integer defesa) {
+		this.time = time;
+		this.pontos = pontos;
+		this.ataque = ataque;
+		this.defesa = defesa;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public Integer getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(Integer pontos) {
+		this.pontos = pontos;
+	}
+
+	public Integer getAtaque() {
+		return ataque;
+	}
+
+	public void setAtaque(Integer ataque) {
+		this.ataque = ataque;
+	}
+
+	public Integer getDefesa() {
+		return defesa;
+	}
+
+	public void setDefesa(Integer defesa) {
+		this.defesa = defesa;
+	}
+	
+	@Override
+	public String toString() {
+		return time + ", " + pontos + " pontos, " + ataque + " gols marcados" + defesa + " gols sofridos";
+	}
+	
+	
+}
---
+++ src/programa/Programa.java
@@ -0,0 +1,15 @@
+package programa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dados.Times;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		List<Times>list = new ArrayList<>();
+	}
+
+}
